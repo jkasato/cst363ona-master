98b317897bd3549633a57fa4cadf00f187ed2cb1
jkasato <32782614+jkasato@users.noreply.github.com>
2019-03-01 12:55:31 -0800

    project2

--- cst363ona/src/mpclient1.py
+++ cst363ona/src/mpclient1.py
@@ -14,44 +14,20 @@ for i in range(1, 101):
     for a in range(1,400):
         studentid=a+4
     '''
-    #Major should be random selection of ‘Biology’, ‘Business’, ‘CS’, ‘Statistics’
+    # Major should be random selection of ‘Biology’, ‘Business’, ‘CS’, ‘Statistics’
     if i % 2:
-        major='Biology'
+        major = 'Biology'
     elif i % 3:
-        major='Statistics'
+        major = 'Statistics'
     elif i % 5:
-        major='CS'
+        major = 'CS'
     else:
-        major='Business'
-    #gpa is a random value from 2.5 to 4.0
-    gpa=round(random.uniform(2.5, 4.0), 2)
-    #Table: student (id int primary key,  name char(20),  major char(10), double gpa)
-    line = str(studentid)+', "'+name+'", '+'"'+major+'", '+str(gpa)+'\n'
+        major = 'Business'
+    # gpa is a random value from 2.5 to 4.0
+    gpa = round(random.uniform(2.5, 4.0), 2)
+    # Table: student (id int primary key,  name char(20),  major char(10), double gpa)
+    line = str(studentid) + ', "' + name + '", ' + '"' + major + '", ' + str(gpa) + '\n'
 
-    # Verify that the cluster is working by doing the queries in mpclient1.py
-    # '''
-    c = mpclient.Coordinator()
-    # select * from student where id=2  -- 2 is an id that exists
-    print("test1")
-    c.getRowByKey("select * from student where studentid=" + str(studentid), '2')
-    if str(studentid) == '2':
-        print("two is present")
-    print("test2")
-    # select * from student where id=yyyy  -- yyyy is an id that does not exist
-    c.getRowByKey("select * from student where studentid=" + str(studentid), '150')
-    if str(studentid) == '150':
-        print("150 is not present")
-    print("test3")
-    # select * from student where major = ‘Biology’
-    c.getRowByKey("select * from student where major=" + str(major), 'Biology')
-    if major=='Biology':
-        print("we have bio majors")
-    print("test4")
-    # select * from student where gpa >= 3.2
-    c.getRowByKey("select * from student where gpa>=" + str(gpa), 3.20)
-    if gpa>=3.20:
-        print("we have honor roll")
-    #'''
     f.write(line)
 f.close()
 
@@ -59,5 +35,30 @@ c = mpclient.Coordinator()
 c.sendToAll("drop table if exists student")
 c.sendToAll("create table student (studentid int primary key, name char(20), major char(10),gpa double)")
 c.loadTable("student","student.data")
+
+# Verify that the cluster is working by doing the queries in mpclient1.py
+# '''
+c = mpclient.Coordinator()
+# select * from student where id=2  -- 2 is an id that exists
+print("test1")
+c.getRowByKey("select * from student where studentid=" + str('2'), '2')
+if str(studentid) == '2':
+    print("two is present")
+print("test2")
+# select * from student where id=yyyy  -- yyyy is an id that does not exist
+c.getRowByKey("select * from student where studentid=" + str('150'), '150')
+if str(studentid) == '150':
+    print("150 is not present")
+print("test3")
+# select * from student where major = ‘Biology’
+c.getRowByKey("select * from student where major=" + str('Biology'), 'Biology')
+if major=='Biology':
+    print("we have bio majors")
+print("test4")
+# select * from student where gpa >= 3.2
+c.getRowByKey("select * from student where gpa>=" + str(gpa), 3.20)
+if gpa>=3.20:
+    print("we have honor roll")
+    #'''
 
 c.close()
